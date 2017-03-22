c41680f1c2417c2904ea641f0e18cd05f543040f
Dmitry Ulyanov <dmitry.ulyanov.msu@gmail.com>
2017-03-23 02:10:40 +0300

    fix gaussian

--- exp_family.py
+++ exp_family.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 def _dot(a, b):
     return sum(np.inner(x.ravel(), y.ravel()) for x, y in zip(a, b))
 
@@ -7,19 +8,19 @@ def _dot(a, b):
 class ExponentialFamilyDistribution(object):
     def __init__(self, *theta):
         self.theta = theta
-        
+
     def A(self):
         return NotImplemented
-    
+
     def T(self, x):
         return NotImplemented
-    
+
     def h(self, x):
         return NotImplemented
-    
+
     def pdf(self, x):
         return self.h(x) * np.exp(_dot(self.theta, self.T(x)) - self.A())
-    
+
     def sample(self):
         raise NotImplemented
 
@@ -27,25 +28,31 @@ class ExponentialFamilyDistribution(object):
 class GaussianDistribution(ExponentialFamilyDistribution):
     def __init__(self, *theta):
         super(GaussianDistribution, self).__init__(*theta)
-        #print theta[0]
+
         m, S = theta
         self.m = m
         self.S = S
         self.cov = np.linalg.pinv(S)
         self.mu = self.cov.dot(m)
         self.k = m.shape[0]
-    
+
     def A(self):
         return 0.5 * (np.inner(self.m, self.cov.dot(self.m)) - np.log(np.linalg.det(self.S)))
-    
+
     def grad_A(self):
-        return (self.mu, -0.5 * (np.outer(self.mu, self.mu) + self.cov))
-    
+        # \nabla_S - log(det(S)) = - S^{-1} = - cov
+        # \nabla_S^{-1} m^T S^{-1} m =  mm^T
+        # \nabla_S m^T S^{-1} m =  S^{-1} mm^T S^{-1}
+
+        # !!!!! there is "+ cov" in the paper, not "- cov" for some reason
+        t = self.cov.dot(self.m)
+        return (t, -0.5 * (np.outer(t, t) + self.cov))
+
     def T(self, x):
         return (x, -0.5 * np.outer(x, x))
-    
+
     def h(self, x):
         return (2 * np.pi) ** (-self.k / 2.)
-        
+
     def sample(self):
         return np.random.multivariate_normal(mean=self.mu, cov=self.cov)
--- optimizers.py
+++ optimizers.py
@@ -2,67 +2,70 @@ from tqdm import tqdm_notebook
 import numpy as np
 
 
-####### Projected gradient method ####### 
+####### Projected gradient method #######
 
 def step(old_theta, step_size, grad):
     return tuple(t - step_size * g for t, g in zip(old_theta, grad))
 
+
 def projected_gradient(obj, f_class, projector, C, theta0, n_iters=1000):
     estimations = []
     thetas = [theta0]
-    
+
     cumsum = 0
     theta = theta0
     for i in tqdm_notebook(range(1, n_iters + 1)):
         f = f_class(*theta)
         x = f.sample()
-        
+        # print(obj(x))
         cumsum += obj(x) / f.pdf(x)
         estimations.append(cumsum / i)
-        
+
         mult = (obj(x) / f.pdf(x)) ** 2
-        g = tuple(mult * (a - b) for a, b in zip(f.grad_A(), f.T(x)))        
-        theta = projector(*step(theta, C / i ** 0.5, g))
+        g = tuple(mult * (a - b) for a, b in zip(f.grad_A(), f.T(x)))
+        theta = projector(*step(theta, C, g))
         thetas.append(theta)
-        
+
     return thetas, estimations
 
 
+####### Mirror descent method #######
 
-####### Mirror descent method ####### 
-
-def mirror (x, g, alpha, dgf = 'xlogx'):
+def mirror(x, g, alpha, dgf='xlogx'):
     n = len(x)
     updated_x = [x[i] * np.exp(- alpha * g[i]) for i in range(n)]
     return updated_x
 
+
 def mirror_update(obj, f_class, mirror, C, theta0, n_iters=5000):
     estimations = []
     thetas = [theta0]
-    
+
     cumsum = 0
     theta = theta0
-    #print theta
+    # print theta
     for i in tqdm_notebook(range(1, n_iters + 1)):
         f = f_class(*theta)
         x = f.sample()
         cumsum += obj(x) / f.pdf(x)
         estimations.append(cumsum / i)
-        
+
         tmp = (obj(x) / f.pdf(x)) ** 2
         g = tuple(tmp * (a - b) for a, b in zip(f.grad_A(), f.T(x)))
         alpha = C / i ** 0.5
-        theta = mirror(theta, g[0], alpha) #  - C / i ** 0.5 * 
+        theta = mirror(theta, g[0], alpha)  # - C / i ** 0.5 *
         thetas.append((theta, ))
-        
+
     return thetas, estimations
 
 
-####### ADMM method ####### 
+####### ADMM method #######
 import scipy as sp
+
 
 def __ravel(x):
     return np.concatenate([c.ravel() for c in x])
+
 
 def __reshape(x, shapes):
     result = []
@@ -72,34 +75,32 @@ def __reshape(x, shapes):
         shift += np.sum(shape)
     return tuple(result)
 
+
 def admm(obj, f_class, projector, r, theta0, n_iters=1000):
     raise NotImplemented
 
     estimations = []
     thetas = [theta0]
-    
+
     cumsum = 0
     theta = theta0
     theta_shape = tuple([x.shape for x in theta])
     for i in tqdm_notebook(range(1, n_iters + 1)):
         f = f_class(*theta)
         x = f.sample()
-        
+
         cumsum += obj(x) / f.pdf(x)
         estimations.append(cumsum / i)
-        
+
         def subtask_obj(theta):
             theta = __reshape(theta, theta_shape)
             return obj(theta) ** 2 / f.pdf(theta)
 
         res = scipy.optimize.minimize(subtask_obj, )
 
-
         mult = (obj(x) / f.pdf(x)) ** 2
-        g = tuple(mult * (a - b) for a, b in zip(f.grad_A(), f.T(x)))        
+        g = tuple(mult * (a - b) for a, b in zip(f.grad_A(), f.T(x)))
         theta = projector(*step(theta, C / i ** 0.5, g))
         thetas.append(theta)
-        
+
     return thetas, estimations
-
-
